e0988f7e311547d34f6732ee7d7c141567f43d4e
sec5677-arch <sec5677@gmail.com>
2026-08-07 11:45:40 +0900

    claude_project
    claude_project todo app Frist create

---
+++ claude_project/todo-app/eslint.config.mjs
@@ -0,0 +1,14 @@
+const eslintConfig = {
+  extends: ['next/core-web-vitals', 'next/typescript'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+  },
+}
+
+export default eslintConfig
